28b8f969112f9bfdfccf0e1e34b8e454162cdce2
annt <annt@gft.com>
2022-12-01 16:56:52 +0000

    Done

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,7 +1,36 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Solution {
+    public Solution(){
+
+    }
+
+    public int maxArea(int[] height) {
+
+        int firstHeight = 0;
+        int lastHeight = height.length - 1;
+        int result = 0;
+
+        while(firstHeight < lastHeight){
+
+            int length = lastHeight - firstHeight;
+            int width = Math.min(height[firstHeight], height[lastHeight]);
+            int area = length * width;
+
+            if(result < area){
+
+                result = area;
+            }
+
+            if(height[firstHeight] < height[lastHeight]){
+
+                firstHeight ++;
+            }else{
+
+                lastHeight --;
+            }
+        }
+
+        return result;
     }
 }
--- src/test/java/com/example/SolutionTest.java
+++ src/test/java/com/example/SolutionTest.java
@@ -1,2 +1,48 @@
-package com.example;public class SolutionTest {
+package com.example;
+
+import org.example.Solution;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SolutionTest {
+
+    static Solution solution;
+
+    int result;
+    int[] height;
+
+    @BeforeAll
+    static void setup(){
+
+        solution = new Solution();
+    }
+
+    @Test
+    public void noHeight(){
+
+        result = 0;
+        height = new int[] {0,0};
+
+        assertEquals(result, solution.maxArea(height));
+    }
+
+    @Test
+    public void TwoNumbersInHeight(){
+
+        result = 3;
+        height = new int[] {4,3};
+
+        assertEquals(result, solution.maxArea(height));
+    }
+
+    @Test
+    public void NineNumbersInHeight(){
+
+        result = 49;
+        height = new int[] {1,8,6,2,5,4,8,3,7};
+
+        assertEquals(result, solution.maxArea(height));
+    }
 }
